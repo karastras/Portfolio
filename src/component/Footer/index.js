f63994f7a688cc76eb583231b325464b4de4af7c
karastras <jerome.burrus.oc@gmail.com>
2021-05-15 16:24:20 +0200

    fix responsive bugs on iphone5

--- src/component/Footer/index.js
+++ src/component/Footer/index.js
@@ -9,13 +9,15 @@ import './style.scss';
 const Footer = () => {
     return (
         <div className="footer">
-            <div className="footer-content">                
-                <a className="footer-linked" target='_blank' rel='noreferrer' href="https://www.linkedin.com/in/j%C3%A9r%C3%B4me-burrus-28855b192/">
-                    <FaLinkedin />
-                </a>
-                <a className="footer-Gmail" href="mailto:burrus.jerome.oc@gmail.com">
-                    <CgMail />
-                </a>
+            <div className="footer-content">
+                <div className="footer-icons">
+                    <a className="footer-linked" target='_blank' rel='noreferrer' href="https://www.linkedin.com/in/j%C3%A9r%C3%B4me-burrus-28855b192/">
+                        <FaLinkedin />
+                    </a>
+                    <a className="footer-Gmail" href="mailto:burrus.jerome.oc@gmail.com">
+                        <CgMail />
+                    </a>
+                </div>               
                 <div>
                     <p className="footer-text">
                         E-mail:&nbsp;
